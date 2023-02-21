76dde9bb2df63791cfec9224dae7836d3ccaf125
ZaphireLiu <liuhan.1631@gmail.com>
2023-02-21 21:19:19 +0800

    天气数据代码更新：下载部分基本完成 数据截取还没做

--- src/getClimData.py
+++ src/getClimData.py
@@ -8,14 +8,21 @@
 # `numpy`, `pandas`, `tqdm`
 # 
 # #### 代码
+# 
+# 此文件主要用于调试，运行时使用`getClimData.py`文件，该文件会阶段性与此文件同步
 
 # %%
-# 初始化
-import os
+import os, gzip
+import netCDF4
 import numpy as np
 import pandas as pd
 import ftplib as ftp
-from tqdm import tqdm
+from math import ceil
+from tqdm import tqdm as tqdmCMD
+from tqdm.gui import tqdm as tqdmGUI
+
+# %%
+# 定义函数、重载类
 
 def isSavedFile(fileName: str, path:str):
     """
@@ -31,8 +38,56 @@ def isSavedFile(fileName: str, path:str):
     fileList = [i.split('.csv') for i in os.listdir(path) if '.csv' in i]
     return fileName in fileList
 
+def unpack_gz(src, dst):
+    g_file = gzip.GzipFile(src)
+    c_file = open(dst, "wb+")
+    c_file.write(g_file.read())
+    c_file.close()
+    g_file.close()
+
+# ftplib中为了增加对TLS支持所以加了一个sslSocket的判断，理论上在这里运行一遍应该能解决下面未定义的错误
+try:
+    import ssl
+except ImportError:
+    _SSLSocket = None
+else:
+    _SSLSocket = ssl.SSLSocket
+
+class myFTP(ftp.FTP):
+
+    def retrbinary(self, cmd, callback, blocksize=8192, rest=None):
+        """Retrieve data in binary mode.  A new port is created for you.
+
+        ### Args:
+          `cmd`: A RETR command.
+          `callback`: A single parameter callable to be called on each
+                    block of data read.
+          `blocksize`: The maximum number of bytes to read from the
+                     socket at one time.  [default: `8192`]
+          `rest`: Passed to transfercmd().  [default: `None`]
+
+        ### Returns:
+          The response code.
+        """
+        self.voidcmd('TYPE I')
+        fileSize = self.size(cmd.replace('retr ', '')) # 1024进制，单位b
+        progBar  = tqdmCMD(total=fileSize)
+        with self.transfercmd(cmd, rest) as conn:
+            while 1:
+                data = conn.recv(blocksize)
+                if not data:
+                    progBar.close()
+                    break
+                callback(data)
+                progBar.update(blocksize)
+            # shutdown ssl layer
+            if _SSLSocket is not None and isinstance(conn, _SSLSocket):
+                conn.unwrap()
+        return self.voidresp()
+
+# %%
 # 连接ftp站点
-ftpSite = ftp.FTP()
+ftpSite = myFTP()
 ftpSite.connect(host="ftp2.tpdc.ac.cn", port=6201)
 ftpSite.login(user="download_38710462", passwd="59317538")
 
@@ -49,26 +104,35 @@ print(ftpSite.getwelcome())
 # %%
 # 参数设置
 
-dataPath  = '../data/climData/'         # 数据保存路径
-cachePath = '../data/climData/cache/'   # 压缩包和nc文件暂存路径
-timeRange = [199201,202312]             # 时间段
-latRange  = [36.45, 38.45]              # latitude Range
-lonRange  = [119.55, 122.05]            # longitude Range
+dataPath   = '../data/climData/'         # 数据保存路径
+cachePath  = '../data/climData/cache/'   # 压缩包和nc文件暂存路径
+timeRange  = [199201,202312]             # 时间段
 
-# %% [markdown]
-# 注意：**不建议**在notebook内直接运行下面一段代码，貌似jupyter对进度显示的支持度不太行
+# 注意经纬度以0.1度为单位，每个结尾有个0.05的偏移
+latRange   = [36.45, 38.45]              # latitude Range 
+lonRange   = [119.55, 122.05]            # longitude Range
 
-# %%
-pathName   = 'Data_forcing_03hr_010deg/'
+infoPrint  = False                       # 是否在命令行内输出信息
+pathName   = 'Data_forcing_03hr_010deg/' # FTP站点的目标文件夹
 folderList = ['Temp', 'Prec', 'Pres', 'SRad', 'SHum', 'LRad', 'Wind']
 
+# %%
 withIn     = lambda val, range: True if val >= min(range) and val <= max(range) else False
-
 fileCount  = 0
 for folderName in folderList:
     path      = pathName + folderName + '/'
     fileCount += len(ftpSite.nlst(path)[2:])
-pbar = tqdm(total=fileCount)
+
+# %% [markdown]
+# 注意：**不建议**在notebook内直接运行下面一段代码，貌似jupyter对GUI进度显示的支持度不太行  
+# 因为还有别的进度条也不好做俩命令行的进度条
+
+# %%
+pbar      = tqdmGUI(total=fileCount)
+def pbarPrint(info: str, isPrint = False):
+    pbar.set_description(info)
+    if isPrint:
+        pbar.write(info)
 
 for folderName in folderList:
     path     = pathName + folderName + '/'
@@ -77,24 +141,29 @@ for folderName in folderList:
         fileName = file.split('.nc.gz')[0]
         time = int(fileName[-6:])
         if not withIn(time, timeRange):
-            pbar.write('{} not in range, skipped.'.format(fileName))
+            pbarPrint('{}\nnot in range, skipped\n'.format(fileName), infoPrint)
             pbar.update(1)
             continue
         elif isSavedFile(fileName, dataPath):
-            pbar.write('{} is already saved.'.format(fileName))
+            pbarPrint('{}\nalready saved\n'.format(fileName), infoPrint)
             pbar.update(1)
             continue
-        # else:
-        #     ftpSite.retrbinary('retr {}'.format(path + file), \
-        #         open(cachePath + 'cache.nc.gz', 'wb').write)
-        #     pbar.write
+        pbarPrint('{}\ndownloading\n'.format(fileName), infoPrint)
+        ftpSite.retrbinary('retr {}'.format(path + file), \
+            open(cachePath + 'cache.nc.gz', 'wb').write)
+        pbarPrint('{}\nprocessing\n'.format(fileName), infoPrint)
+        unpack_gz(cachePath + 'cache.nc.gz', cachePath + 'cache.nc')
+        data = netCDF4.Dataset(cachePath + 'cache.nc')
+        print(data.variables.keys())
+        data.close()
+        # os.remove(cachePath + 'cache.nc.gz')
+        # os.remove(cachePath + 'cache.nc')
+        pbar.update(1)
+
+
 
 # %%
-# from time import sleep
-# pbarT = tqdm(total=100)
-# for i in range(100):
-#     pbarT.update(i)
-#     sleep(0.5)
-# pbarT.close()
+# 退出FTP站点
+ftpSite.quit()
 
 
